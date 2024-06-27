b9bc62b96c244b0fa3ecc9ccc7381b01a875257f
Antonia Frey <antonia.frey@devoteam.com>
2024-06-27 20:24:27 +0200

    Added deploy.yml file

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/polly/', // Adjust to match your GitHub Pages repository path
-  build: {
-    outDir: 'dist', // Default output directory for Vite builds
-  },
-})
+  base: "/polly/",
+});
+
