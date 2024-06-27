001320fbe3e410bcb02b13338f10a74d1a9fbef9
Antonia Frey <antonia.frey@devoteam.com>
2024-06-27 20:13:54 +0200

    Fix for github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/polly/', // Adjust to match your GitHub Pages repository path
+  build: {
+    outDir: 'dist', // Default output directory for Vite builds
+  },
 })
